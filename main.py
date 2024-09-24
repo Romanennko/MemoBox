91cb50ea843839bc62149db79709b92c479d7b0d
Valentino <nikiromanen@gmail.com>
2024-09-24 18:40:00 +0200

    Added an application icon and set the position, size of the window

--- main.py
+++ main.py
@@ -1,6 +1,15 @@
 from kivy.app import App
+from kivy.config import Config
+from kivy.core.window import Window
 
 from kivy.uix.screenmanager import Screen, ScreenManager
+
+
+Config.set('kivy', 'window_icon', 'C:\Projects\PyCharm\MemoBox\img\icon.png')
+Window.size = (1024, 768)
+Window.top = 150
+Window.left = 450
+
 
 
 class StartMenu(Screen):
